05864469f7b83d6b4050edab8166d61cb9d79a28
Anthony Romrell <anthonyromrell@mac.com>
2020-02-04 13:03:04 -0700

    Bool Updates

---
+++ CharacterProject/Assets/Scripts/CheckBoolData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CheckBoolData : MonoBehaviour
+{
+    public BoolData boolDataObj;
+    // Update is called once per frame
+    void Update()
+    {
+        if (boolDataObj.value)
+        {
+            print(true);
+            //work to do
+        }
+        else
+        {
+            //do false work
+        }
+    }
+}
